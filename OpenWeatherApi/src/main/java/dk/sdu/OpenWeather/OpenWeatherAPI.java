3dd115bc4c965d07d781ee57836f4c3233698ec2
AndreasLC <andreaslykkecaspers@gmail.com>
2026-05-05 11:29:39 +0200

    added dependencies in pom and requires to common in module-info.java. Made skeleton class OpenWeatherAPI that implements IWeather.

---
+++ OpenWeatherApi/src/main/java/dk/sdu/OpenWeather/OpenWeatherAPI.java
@@ -0,0 +1,45 @@
+﻿package dk.sdu.OpenWeather;
+
+import dk.sdu.scs.common.services.IWeather;
+
+public class OpenWeatherAPI implements IWeather {
+    @Override
+    public void getAll(double lat, double lon) {
+
+    }
+
+    @Override
+    public void requestAPI() {
+
+    }
+
+    @Override
+    public double getTemperature() {
+        return 0;
+    }
+
+    @Override
+    public double getFeelsLikeTemperature() {
+        return 0;
+    }
+
+    @Override
+    public double getHumidity() {
+        return 0;
+    }
+
+    @Override
+    public double getWindSpeed() {
+        return 0;
+    }
+
+    @Override
+    public int getWindDirection() {
+        return 0;
+    }
+
+    @Override
+    public String getCloudCover() {
+        return "";
+    }
+}
---
+++ OpenWeatherApi/src/main/java/module-info.java
@@ -0,0 +1,3 @@
+﻿module OpenWeatherApi {
+    requires dk.sdu.scs.common;
+}
