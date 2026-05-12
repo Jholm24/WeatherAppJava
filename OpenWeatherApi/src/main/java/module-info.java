cd50685bc08a74075bae2854304a776534aba368
AndreasLC <andreaslykkecaspers@gmail.com>
2026-05-12 09:15:53 +0200

    uses geoLocation

--- OpenWeatherApi/src/main/java/module-info.java
+++ OpenWeatherApi/src/main/java/module-info.java
@@ -1,6 +1,8 @@
 ﻿import dk.sdu.scs.common.services.IWeather;
 import dk.sdu.OpenWeather.OpenWeatherAPI;
+import dk.sdu.scs.common.services.IGeoLocation;
 module OpenWeatherApi {
+    uses IGeoLocation;
     requires dk.sdu.scs.common;
     requires java.net.http;
     requires org.json;
