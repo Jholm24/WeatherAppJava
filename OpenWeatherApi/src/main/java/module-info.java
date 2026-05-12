7dc56983772f3072527a2f93d1b85789aa88445c
AndreasLC <andreaslykkecaspers@gmail.com>
2026-05-12 08:58:26 +0200

    provides interface IWeather with OpenWeatherAPI

--- OpenWeatherApi/src/main/java/module-info.java
+++ OpenWeatherApi/src/main/java/module-info.java
@@ -1,5 +1,8 @@
-﻿module OpenWeatherApi {
+﻿import dk.sdu.scs.common.services.IWeather;
+import dk.sdu.OpenWeather.OpenWeatherAPI;
+module OpenWeatherApi {
     requires dk.sdu.scs.common;
     requires java.net.http;
     requires org.json;
+    provides IWeather with OpenWeatherAPI;
 }
